8c43c01abf3530b5cab3751c9cd70cbda477997b
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-13 13:34:35 +0100

    D

--- index.test.js
+++ index.test.js
@@ -3,6 +3,6 @@ const { calculator1 } = require("./index");
 describe("calculator1", () => {
     //1.1
     test("has no arguments in function parameter", () => {
-        expect(calculator1).
+        expect(calculator1).//HAS A PARAMETER OR NOT!!!!!!!!!!!
     });
 });
