1eb704299523c73466394dde6d04bd7dd42e8af4
Leo Volwahsen <volwahsenl@gmail.com>
2022-12-13 14:13:04 +0100

    d

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function calculator1(val) {
+    //1.1
     if (val === undefined) return false;
+    //1.2
+    if (val === Number) {
+        let val2 = val * val;
+        return val2;
+    }
     return val;
 }
 
--- index.test.js
+++ index.test.js
@@ -3,6 +3,11 @@ const { calculator1 } = require("./index");
 describe("calculator1", () => {
     //1.1
     test("has no arguments in function parameter", () => {
-        expect(calculator1).//HAS A PARAMETER OR NOT!!!!!!!!!!!
+        expect(calculator1()).toBe(false);
+    });
+
+    //1.2
+    test("parameter is number and multiplyed by it self", () => {
+        expect(calculator1()).toBe(val);
     });
 });
